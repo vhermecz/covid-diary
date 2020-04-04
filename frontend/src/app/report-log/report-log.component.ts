44fd0c5c9931455f623f195448f64998f3579d67
Lóránt Pintér <lorant.pinter@gmail.com>
2020-04-05 00:56:01 +0200

    Add some more example data

--- frontend/src/app/report-log/report-log.component.ts
+++ frontend/src/app/report-log/report-log.component.ts
@@ -10,12 +10,24 @@ import { Report } from '../report';
 export class ReportLogComponent implements OnInit {
 
   reports: Report[] = [{
-    date: { year: 2020, month: 4, day: 5 },
+    date: { year: 2020, month: 4, day: 1 },
     symptoms: [],
-    comments: "Komment-only"
+    comments: "Ma elkezdtem írni a naplót! Egyelőre kutya bajom"
   }, {
-    date: { year: 2020, month: 4, day: 5 },
-    symptoms: ["dry-cough", "fatigue", "headache"],
+    date: { year: 2020, month: 4, day: 2 },
+    symptoms: ["dry-cough"],
+    comments: "Hm, valami nincs teljesen rendben."
+  }, {
+    date: { year: 2020, month: 4, day: 3 },
+    symptoms: ["fatigue", "headache"],
+    comments: "Eléggé rosszul éreztem ma magam, felkelni is nehéz volt."
+  }, {
+    date: { year: 2020, month: 4, day: 4 },
+    symptoms: [],
+    comments: "Ma voltam a téren, találkoztam Bélával. Szúr az oldalam."
+  }, {
+    date: { year: 2020, month: 4, day: 4 },
+    symptoms: ["fatigue", "headache", "dry-cough"],
     comments: ""
   }];
 
