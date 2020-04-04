a8e73ad726469f758550c9e9e579d08d7ec00480
Lóránt Pintér <lorant.pinter@gmail.com>
2020-04-04 22:25:22 +0200

    Polish code

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,17 +1,21 @@
+// Angular modules
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-import { environment } from '../environments/environment';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ServiceWorkerModule } from '@angular/service-worker';
 
+// Font Awesome
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faCalendar, faComments, faThermometer } from '@fortawesome/free-solid-svg-icons';
+
+// Application components
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { ReportFormComponent } from './report-form/report-form.component';
 import { WelcomeComponent } from './welcome/welcome.component';
-import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { faCalendar, faComments, faThermometer } from '@fortawesome/free-solid-svg-icons';
 
 const appRoutes: Routes = [
   {
@@ -34,6 +38,7 @@ const appRoutes: Routes = [
   imports: [
     BrowserAnimationsModule,
     BrowserModule,
+    FontAwesomeModule,
     FormsModule,
     NgbModule,
     RouterModule.forRoot(
@@ -43,7 +48,6 @@ const appRoutes: Routes = [
       } // <-- debugging purposes only
     ),
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
-    FontAwesomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
