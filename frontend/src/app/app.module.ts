55d40b92dc2186b36021f3367645dfba17465b6a
Lóránt Pintér <lorant.pinter@gmail.com>
2020-04-04 13:31:06 +0200

    Polish code

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SymptomsComponent } from './symptoms/symptoms.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
-import { WelcomeComponent } from './welcome/welcome.component';
 import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './app.component';
+import { SymptomsComponent } from './symptoms/symptoms.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 const appRoutes: Routes = [
   {
