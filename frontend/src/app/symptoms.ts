e429331d31c57c7938389b9296f84c48ffd7f1df
Lóránt Pintér <lorant.pinter@gmail.com>
2020-04-04 22:23:10 +0200

    Rename Symptom.hu to title

--- frontend/src/app/symptoms.ts
+++ frontend/src/app/symptoms.ts
@@ -1,20 +1,20 @@
 export class Symptom {
-  public id: string;
-  public hu: string;
+  public readonly id: string;
+  public readonly title: string;
 }
 
 export const symptoms: Symptom[] = [
-  { id: "high-fever", hu: "Láz" },
-  { id: "low-fever", hu: "Hőemelkedés" },
-  { id: "fatigue", hu: "Levertség" },
-  { id: "cold-shivers", hu: "Hidegrázás" },
-  { id: "dry-cough", hu: "Száraz köhögés" },
-  { id: "wet-cough", hu: "Produktív köhögés" },
-  { id: "sore-throat", hu: "Torokfájás" },
-  { id: "runny-nose", hu: "Orrfolyás" },
-  { id: "dyspnoea", hu: "Nehézlégzés" },
-  { id: "headache", hu: "Fejfájás" },
-  { id: "joint-pains", hu: "Izületi és végtagfájdalmak" },
-  { id: "diarrhea", hu: "Hasmenés, hasi panaszok" },
-  { id: "lack-of-smell", hu: "Csökkent szaglás, ízlelés" },
+  { id: "high-fever", title: "Láz" },
+  { id: "low-fever", title: "Hőemelkedés" },
+  { id: "fatigue", title: "Levertség" },
+  { id: "cold-shivers", title: "Hidegrázás" },
+  { id: "dry-cough", title: "Száraz köhögés" },
+  { id: "wet-cough", title: "Produktív köhögés" },
+  { id: "sore-throat", title: "Torokfájás" },
+  { id: "runny-nose", title: "Orrfolyás" },
+  { id: "dyspnoea", title: "Nehézlégzés" },
+  { id: "headache", title: "Fejfájás" },
+  { id: "joint-pains", title: "Izületi és végtagfájdalmak" },
+  { id: "diarrhea", title: "Hasmenés, hasi panaszok" },
+  { id: "lack-of-smell", title: "Csökkent szaglás, ízlelés" },
 ];
