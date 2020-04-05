f60235dcd20b5536f42a5b4d90744911bc1a03a8
Gabor Guta <gaboo@fastmail.fm>
2020-04-05 17:34:54 +0200

    REST API fixed

--- backend/diarybase/admin.py
+++ backend/diarybase/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Reporter, Symptom, EpidemicActivity
+from .models import Reporter, Symptoms, EpidemicActivity
 # Register your models here.
 admin.site.register(Reporter)
-admin.site.register(Symptom)
+admin.site.register(Symptoms)
 admin.site.register(EpidemicActivity)
